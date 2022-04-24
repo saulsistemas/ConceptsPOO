8764ffd278ee6e61b884f1bca2adb2867c66a404
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-04-23 19:26:32 -0500

    Finish date Class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using ConceptsPOO;
+
+//Date date1 = new Date(2022,2,11);
+
+try
+{
+    Console.WriteLine(new Date(2024, 12, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(2024, 2, 29));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
